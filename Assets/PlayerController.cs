beec04d1b9014df3dc88b59ee5ac7f547e4a17c7
LeoEras <leoded94@hotmail.com>
2016-12-15 01:06:12 -0500

    Pruebas de movimiento y sprites

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public int facing_direction;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+		animator.SetInteger("direction", facing_direction);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+
+		if (vertical > 0)
+		{
+			animator.SetInteger("direction", 0);
+		}
+		else if (vertical < 0)
+		{
+			animator.SetInteger("direction", 2);
+		}
+		else if (horizontal > 0)
+		{
+			animator.SetInteger("direction", 1);
+		}
+		else if (horizontal < 0)
+		{
+			animator.SetInteger("direction", 3);
+		}
+
+		if (vertical != 0 || horizontal != 0) {
+			animator.SetBool ("is_moving", true);
+		} else {
+			animator.SetBool ("is_moving", false);
+		}
+
+		transform.Translate (Input.GetAxisRaw ("Horizontal") * speed,
+			Input.GetAxisRaw ("Vertical") * speed, 0);
+	}
+}
